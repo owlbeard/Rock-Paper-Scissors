d0d00b5947a105d5e443980590fb7b7686b0bee3
Owlbeard <omeraltun96@gmail.com>
2023-03-15 03:22:32 +0300

    Git is confusing

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,52 +1,91 @@
-console.log("Welcome to Rock-Paper-Scissors");
-console.log("The rules are:");
-console.log("1)Rock demolishes scissors.");
-console.log("2)Paper suffocates rock.");
-console.log("3)Scissors mutilate paper.");
-
 let wins = 0;
 let losses = 0;
 let draws = 0;
+let userChoice = "";
 
+const choices = document.querySelector(".choices");
+const result = document.querySelector(".result");
+const results = document.querySelector(".results");
+const declare = document.querySelector(".declaration");
 
-while (true) {
-  console.log(`Wins: ${wins} Losses: ${losses} Draws: ${draws}`);
-  console.log("Make your move: (R)ock, (P)aper, (S)cissors, or (E)xit");
-  let userChoice = prompt().toUpperCase();
-  let compChoices = ["R", "P", "S"];
+declare.textContent = "Make your move: (R)ock, (P)aper, or (S)cissors";
+
+function compMove() {
+  const compChoices = ["R", "P", "S"];
   let randChoice = Math.floor(Math.random() * compChoices.length);
   let compChoice = compChoices[randChoice];
+  return compChoice
+}
 
-
+function playRound() {
+  let compChoice = compMove();
   if (compChoice == "R") {
-    console.log('Computer came up with the choice of "Rock"');
+    choices.textContent = 'Computer came up with the choice of "Rock"';
   }
   else if (compChoice == "P") {
-    console.log('Computer came up with the choice of "Paper"');
+    choices.textContent = 'Computer came up with the choice of "Paper"';
   }
   else if (compChoice == "S") {
-    console.log('Computer came up with the choice of "Scissors"');
+    choices.textContent ='Computer came up with the choice of "Scissors"';
   }
-
+  else {
+    return
+  }
   if (userChoice == compChoice) {
-    console.log(`The computer came up with: ${compChoice}`);
-    console.log("It's a draw!");
+    result.textContent = "It's a draw!";
     draws = draws + 1
   } 
   else if ((userChoice == "R" && compChoice == "P") || (userChoice == "P" && compChoice == "S") || (userChoice == "S" && compChoice == "R")) {
-    console.log(`The computer came up with: ${compChoice}`);
-    console.log("You lost miserably.");
+    result.textContent = "You lost miserably.";
     losses = losses + 1
   } 
   else if ((userChoice == "R" && compChoice == "S") || (userChoice == "P" && compChoice == "R") || (userChoice == "S" && compChoice == "P")) {
-    console.log(`The computer came up with: ${compChoice}`);
-    console.log("Congratulations! You have won!!!");
+    result.textContent = "Congratulations! You have won!!!";
     wins = wins + 1
   }
-  else if (userChoice == "E" || userChoice == "e") {
-    throw Error("So you have chosen the death of me :(");
+  else {
+    return
+  }
+  results.textContent = `Wins: ${wins} Losses: ${losses} Draws: ${draws}`;
+}
+
+window.addEventListener("keydown", function clicking(e) {
+  userChoice = `${e.key}`.toUpperCase();
+  if (userChoice === "R" || userChoice === "P" || userChoice === "S") {
+    playRound();
+    if (wins === 5) {
+      this.window.removeEventListener("keydown", clicking);
+      results.textContent = "WOOHOO!!! YOU HAVE WON THE GAME!"
+    }
+    if (losses === 5) {
+      this.window.removeEventListener("keydown", clicking);
+      results.textContent = "Bummer! You have lost the game :("
+    }
+  }
+});
+
+const images = document.querySelector(".images")
+images.addEventListener("click", function buttoning(e) {
+  let target = e.target.className;
+  if (target === "rock") {
+    userChoice = "R";
+  }
+  else if (target === "paper") {
+    userChoice = "P";
+  }
+  else if (target === "scissors") {
+    userChoice = "S";
   }
   else {
-    console.log("Please press either: (R)ock, (P)aper, (S)cissors, or (E)xit");
+    return;
   }
-}
+  playRound();
+  if (wins === 5) {
+    images.removeEventListener("click", buttoning);
+    results.textContent = "WOOHOO!!! YOU HAVE WON THE GAME!"
+  }
+  if (losses === 5) {
+    images.removeEventListener("click", buttoning);
+    results.textContent = "Bummer! You have lost the game :("
+  }
+});
